768ef8d6f0ab9200735aea0b5ca6f565b6073ecd
Gustavo Adolfo Tobías Ramírez <tobiasgusito@outlook.com>
2026-07-26 12:02:45 -0600

    Cambio de prueba de publicar

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import { Main } from '../components/Main';
 import { Login } from '../pages/Login';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
     element: <Main />,
